c5c85868d094537554b3122052e4bdabcefe7933
Naveen <lokesh.manchanda@crownstack.com>
2019-09-24 18:51:09 +0530

    inital commit

--- app/src/main/java/com/sample/MainActivity.java
+++ app/src/main/java/com/sample/MainActivity.java
@@ -2,8 +2,13 @@ package com.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+
+import com.devcoder.toast.CustomToast;
+import com.devcoder.toast.FA;
+import com.devcoder.toast.ToastBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +19,25 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_error_toast).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                showToast(1);
             }
         });
     }
-private void showToast()
-{
 
-}
+    public void showToast(int number){
+        String text="Just Some Text";
+        CustomToast.makeText(this,text,CustomToast.LENGTH_SHORT,number,false).show();
+        CustomToast.makeText(this,text,CustomToast.LENGTH_SHORT,number).show();
+        CustomToast customToast=new ToastBuilder(this)
+                .textSize(16)
+                .leftIcon(R.drawable.confusing_shape)
+                .textColor(Color.WHITE)
+                .showTopIcon(true)
+                .topIcon(FA.FA_ANGELLIST)
+                .text("That's An Easy Toast")
+                .type(CustomToast.SUCCESS)
+                .build();
+        customToast.show();
+    }
 
 }
